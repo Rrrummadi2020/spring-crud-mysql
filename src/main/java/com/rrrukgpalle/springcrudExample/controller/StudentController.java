477af2d5b752536bc32cd655c429735cd2a7d0e2
Ummadi Rama Rangeswara Reddy <ramarangeswara.reddy@sen-sei.in>
2022-05-19 08:23:43 +0530

    Using Common annotation  requestMapping in the controller class for class

--- src/main/java/com/rrrukgpalle/springcrudExample/controller/StudentController.java
+++ src/main/java/com/rrrukgpalle/springcrudExample/controller/StudentController.java
@@ -3,29 +3,39 @@ package com.rrrukgpalle.springcrudExample.controller;
 import com.rrrukgpalle.springcrudExample.entity.Student;
 import com.rrrukgpalle.springcrudExample.service.impl.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequestMapping("/api")
 @RestController
 public class StudentController {
 
     @Autowired
     private StudentService studentservice;
 
-    @PostMapping("/api/student")
-    public Student addStudent(@RequestBody Student student){
+    @PostMapping("/student")
+    public Student addStudent(@RequestBody Student student) {
         return studentservice.saveStudent(student);
     }
-    @PostMapping("/api/students")
-    public List<Student> addStudents(@RequestBody List<Student> students){
-        return  studentservice.saveStudents(students);
+
+    @PostMapping("/students")
+    public List<Student> addStudents(@RequestBody List<Student> students) {
+        return studentservice.saveStudents(students);
     }
-    @GetMapping("/api/students")
-    public List<Student> getAllStudents(){
+
+    @GetMapping("/students")
+    public List<Student> getAllStudents() {
         return studentservice.getAllStudents();
+    }
+
+    @PutMapping("/student")
+    public Student updateStudent(@RequestBody Student student) {
+        return studentservice.updateStudent(student);
+    }
+
+    @DeleteMapping("/student")
+    public String deleteStudent(@RequestParam Long id) {
+        return studentservice.deleteById(id);
     }
 }
